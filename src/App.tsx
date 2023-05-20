c4b80e05ab9cb688698c5e8b7a5eb32d8ae0de21
hwinkr <dnddl8280@naver.com>
2023-05-20 18:28:19 +0900

    Feat: HOC 적용을 위한 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,64 @@
+import MajorLayout from '@components/MajorLayout';
 import Announcement from '@pages/Announcement';
 import Home from '@pages/Home';
 import MajorDecision from '@pages/MajorDecision';
 import My from '@pages/My';
 import { Routes, Route } from 'react-router-dom';
 
+interface RouterBase {
+  id: number;
+  path: string;
+  element: React.ReactNode;
+}
+
+interface RouterMajorRequired extends RouterBase {
+  majorRequired: true;
+}
+
+type RouterElement = RouterBase | RouterMajorRequired;
+
+const routerData: RouterElement[] = [
+  {
+    id: 0,
+    path: '/',
+    element: <Home />,
+  },
+  {
+    id: 1,
+    path: '/major-decision',
+    element: <MajorDecision />,
+  },
+  {
+    id: 2,
+    path: '/announcement',
+    element: <Announcement />,
+    majorRequired: true,
+  },
+  {
+    id: 3,
+    path: '/my',
+    element: <My />,
+    majorRequired: true,
+  },
+];
+
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/announcement" element={<Announcement />} />
-      <Route path="/my" element={<My />} />
-      <Route path="/major-decision" element={<MajorDecision />} />
+      {routerData.map((router: RouterElement) => {
+        if ((router as RouterMajorRequired).majorRequired) {
+          return (
+            <Route
+              key={router.id}
+              path={router.path}
+              element={<MajorLayout>{router.element}</MajorLayout>}
+            />
+          );
+        }
+        return (
+          <Route key={router.id} path={router.path} element={router.element} />
+        );
+      })}
     </Routes>
   );
 };
