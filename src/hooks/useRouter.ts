7f0b197a01efc6139158f6d5a64d98c6fe9f5e67
hwinkr <dnddl8280@naver.com>
2023-05-20 18:29:10 +0900

    Feat: userRouter  커스텀 훅에 뒤로가기 버튼 추가

--- src/hooks/useRouter.ts
+++ src/hooks/useRouter.ts
@@ -1,12 +1,32 @@
-import { useNavigate } from 'react-router-dom';
+import { To, useNavigate } from 'react-router-dom';
 
 const useRoter = () => {
   const router = useNavigate();
 
   return {
     currentPath: window.location.pathname,
-    routerTo: (path: string) => router(path),
+    routerTo: (path: To) => router(path),
+    goBack: () => router(-1 as number),
   };
 };
 
 export default useRoter;
+
+/*
+export declare type To = string | Partial<Path>;
+
+export interface Path {
+    
+    /* A URL pathname, beginning with a /.
+     
+    pathname: string;
+    
+     * A URL search string, beginning with a ?.
+    
+    search: string;
+    
+     * A URL fragment identifier, beginning with a #.
+     
+    hash: string;
+}
+*/
